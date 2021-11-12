56097a3a286db06c4339518314564d8a89e39c51
Paulo Salvatore <salvatoregames@gmail.com>
2021-11-12 10:40:26 -0300

    Implementação do endpoint Read Single

--- index.js
+++ index.js
@@ -12,8 +12,23 @@ app.get("/oi", function (req, res) {
 const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
 //               0                   1                2
 
+// [GET] "/herois" - Read All (Ler tudo)
 app.get("/herois", function (req, res) {
     res.send(herois);
+});
+
+// [GET] "/herois/:id" - Read Single By Id (Ler individualmente - pelo Id)
+app.get("/herois/:id", function (req, res) {
+    // Lógica de obtenção do ID
+    const id = +req.params.id - 1;
+
+    // console.log(id, typeof id);
+
+    // Lógica de acesso ao dados
+    const item = herois[id];
+
+    // Lógica de envio desse dado encontrado
+    res.send(item);
 });
 
 app.listen(3000);
